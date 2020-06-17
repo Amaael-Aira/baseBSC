6f4f9e8f237d77a7c832d6a5bf2f8d921db7f4e4
Amaael <amaael@Amaaels-MacBook-Pro.local>
2020-06-17 11:14:09 -0700

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/baseBSC.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/baseBSC.dir/main.cpp.o"
+  "baseBSC"
+  "baseBSC.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/baseBSC.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
